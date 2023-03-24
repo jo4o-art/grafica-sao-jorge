a77aeac8a9602761ad2a8c802dd832ef0ebb869e
João Pedro Oliveira <126018501+jo4o-art@users.noreply.github.com>
2023-03-24 20:26:29 -0300

    criando media query

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function clikmenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
+        itens.style.background = 'none'
     } else {
         itens.style.display = 'block'
+        itens.style.background = '#00000079'
+    }
+}
+
+function tamanho() {
+    if (innerWidth >= 768) {
+        itens.style.display = 'flex'
+    } else {
+        itens.style.display = 'none'
     }
 }
